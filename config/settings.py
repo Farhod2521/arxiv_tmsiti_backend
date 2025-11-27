a004bbeaa9468a0858751ff0596f4f487d0655b6
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-11-27 15:06:44 +0500

    fixed

--- config/settings.py
+++ config/settings.py
@@ -174,7 +174,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 AUTH_USER_MODEL = "app_main.User"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
