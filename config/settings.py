fc4cc985f7bd7e409c18d166988bacce95adfc74
Farhod2521 <abdikarimovfarhod86@gmail.com>
2025-12-04 11:20:45 +0500

    fixed

--- config/settings.py
+++ config/settings.py
@@ -184,3 +184,18 @@ AUTH_USER_MODEL = "app_main.User"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
